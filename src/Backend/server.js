7b2602d2fcfe07f43545e7d865d7c696c6f3ce1b
= <rahul.gudipati321@gmail.com>
2025-05-04 00:14:00 +0530

    packages added

--- src/Backend/server.js
+++ src/Backend/server.js
@@ -20,5 +20,5 @@ app.post('/api/checkout', (req, res) => {
 })
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`)
+  console.log(`Server is running on http://localhost:${PORT}`)
 })
