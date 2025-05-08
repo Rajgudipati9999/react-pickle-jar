0a14d09e9acc39d6b5fb31c0da1b941d691e0c6b
= <rahul.gudipati321@gmail.com>
2025-05-08 12:05:51 +0530

    Link element is added to logo

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -3,11 +3,12 @@ import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import Badge from "@mui/material/Badge";
 import "./index.css";
 
-function Navbar({ cartCount = 10 }) {
+function Navbar() {
   return (
     <nav className="navbar">
-      <img src="refinedlogo.png" alt="logo" className="navbar-logo" />
-
+      <Link to='/'>
+         <img src="refinedlogo.png" alt="logo" className="navbar-logo" />
+     </Link>
       <ul className="navbar-links">
         <li>
           <Link to="/" className="nav-link">Home</Link>
@@ -17,8 +18,8 @@ function Navbar({ cartCount = 10 }) {
         </li>
         <li>
           <Link to="/cart" className="nav-link">
-            <Badge badgeContent={cartCount} color="success">
-              <ShoppingCartIcon />
+            <Badge badgeContent={6} color="success">
+              <ShoppingCartIcon/>
             </Badge>
           </Link>
         </li>
