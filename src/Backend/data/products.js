d43540cfa6652cb4da187b049a69ee7cbdf7b001
= <rahul.gudipati321@gmail.com>
2025-05-13 16:52:01 +0530

    new changes made

--- src/Backend/data/products.js
+++ src/Backend/data/products.js
@@ -1,40 +1,126 @@
-const products = [
+const products = {
+  'non-veg': [
     {
-      id: '1',
+      id: 1,
       name: 'Chicken Boneless Pickle',
       price: 120,
       description: 'Chicken boneless pickle made with traditional methods.',
       image: 'https://res.cloudinary.com/dx8rhno8y/image/upload/v1744713387/chickenboneless_wiyf1j.png',
     },
     {
-      id: '2',
+      id: 2,
       name: 'Chicken Bone Pickle',
       price: 100,
       description: 'Tasty Chicken pickle to spice up your meals.',
       image: 'https://res.cloudinary.com/dx8rhno8y/image/upload/v1744713546/chikenbone_yrtpaz.png',
     },
     {
-        id: '3',
-        name: 'Prawns Pickle',
-        price: 150,
-        description: 'Tasty Prawan pickle to spice up your meals.',
-        image: 'https://res.cloudinary.com/dx8rhno8y/image/upload/v1744713589/prawns_srsxyh.png',
-      },
-      {
-        id: '4',
-        name: 'Fish Pickle',
-        price: 200,
-        description: 'Fish pickle to spice up your meals.',
-        image: 'https://res.cloudinary.com/dx8rhno8y/image/upload/v1744713560/Fish_e8l2sw.png',
-      },
-      {
-        id: '5',
-        name: 'Gongura Chicken Pickle',
-        price: 120,
-        description: 'Tasty Gongura Chicken pickle to spice up your meals.',
-        image: 'https://res.cloudinary.com/dx8rhno8y/image/upload/v1744713572/gongurachicken_hllzss.png',
-      },
-  ]
-  
-  module.exports = products
-  
+      id: 3,
+      name: 'Prawns Pickle',
+      price: 150,
+      description: 'Tasty Prawn pickle to spice up your meals.',
+      image: 'https://res.cloudinary.com/dx8rhno8y/image/upload/v1744713589/prawns_srsxyh.png',
+    },
+    {
+      id: 4,
+      name: 'Fish Pickle',
+      price: 200,
+      description: 'Fish pickle to spice up your meals.',
+      image: 'https://res.cloudinary.com/dx8rhno8y/image/upload/v1744713560/Fish_e8l2sw.png',
+    },
+    {
+      id: 5,
+      name: 'Gongura Chicken Pickle',
+      price: 120,
+      description: 'Tasty Gongura Chicken pickle to spice up your meals.',
+      image: 'https://res.cloudinary.com/dx8rhno8y/image/upload/v1744713572/gongurachicken_hllzss.png',
+    },
+  ],
+
+  'veg': [
+    {
+      id: 6,
+      name:'Mango Pickle',
+      price:80,
+      description:'Mango pickle made with traditional methods.',
+      image:'https://res.cloudinary.com/dx8rhno8y/image/upload/v1744715617/ChatGPT_Image_Apr_5_2025_10_28_28_PM_y9a86d.png'
+    },
+    {
+      id: 7,
+      name:'Gongura Pickle',
+      price:180,
+      description:'Gongura pickle made with traditional methods.',
+      image:'https://res.cloudinary.com/dx8rhno8y/image/upload/v1744715633/ChatGPT_Image_Apr_5_2025_10_32_51_PM_gavvvb.png'
+    },
+    {
+      id: 8,
+      name:'Tomato Pickle',
+      price:70,
+      description:'Tomato pickle made with traditional methods.',
+      image:'https://res.cloudinary.com/dx8rhno8y/image/upload/v1744715653/ChatGPT_Image_Apr_5_2025_10_36_15_PM_cddnw6.png'
+    },
+    {
+      id: 9,
+      name:'Usiri Pickle',
+      price:60,
+      description:'Usiri pickle made with traditional methods.',
+      image:'https://res.cloudinary.com/dx8rhno8y/image/upload/v1744715667/ChatGPT_Image_Apr_5_2025_10_41_04_PM_hkhian.png'
+    },
+    {
+      id: 10,
+      name:'Lemon Pickle',
+      price:80,
+      description:'Lemon pickle made with traditional methods.',
+      image:'https://res.cloudinary.com/dx8rhno8y/image/upload/v1744715679/ChatGPT_Image_Apr_5_2025_10_44_27_PM_ghjgi0.png'
+    },
+     {
+      id: 11,
+      name:'Red Mirchi Pickle',
+      price:80,
+      description:'Red Mirchi pickle made with traditional methods.',
+      image :'https://res.cloudinary.com/dx8rhno8y/image/upload/v1744715685/ChatGPT_Image_Apr_5_2025_10_54_52_PM_i2dyyi.png'
+    },
+     {
+      id: 12,
+      name:'Pudeena Pickle',
+      price:80,
+      description:'Pudeena pickle made with traditional methods.',
+      image:'https://res.cloudinary.com/dx8rhno8y/image/upload/v1744715690/ChatGPT_Image_Apr_5_2025_10_56_27_PM_wfvwjt.png'
+    },
+    
+
+  ],
+
+  'sweets': [
+    {
+      id: 13,
+      name:'Dry Fruits Pootharekulu',
+      price:100,
+      description:'Home Made Pootharekulu.',
+      image:'https://res.cloudinary.com/dx8rhno8y/image/upload/v1744715844/ChatGPT_Image_Apr_6_2025_08_01_12_PM_t4hqbi.png'
+    },
+    {
+      id: 14,
+      name:'Sunni Undalu',
+      price:180,
+      description:'Home made Sunni Undalu.',
+      image:'https://res.cloudinary.com/dx8rhno8y/image/upload/v1744715849/ChatGPT_Image_Apr_6_2025_08_14_21_PM_qji23t.png'
+    },
+    {
+      id: 15,
+      name:'Nuvvula Ariselu',
+      price:120,
+      description:'Home made Nuvvula Ariselu.',
+      image:'https://res.cloudinary.com/dx8rhno8y/image/upload/v1744715859/ChatGPT_Image_Apr_6_2025_08_19_19_PM_k9efrh.png'
+    },
+    {
+      id: 16,
+      name:'Bellam Gavvalu',
+      price:80,
+      description:'Home made Gavvalu.',
+      image:'https://res.cloudinary.com/dx8rhno8y/image/upload/v1744715872/ChatGPT_Image_Apr_6_2025_09_04_43_PM_vgntuh.png'
+    },
+  ],
+};
+
+module.exports = products;
